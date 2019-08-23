9d38d06988afcaff2dd6248534ba24f45586d5a3
Kenia <kenia1001@yahoo.com.mx>
2019-08-23 14:11:12 -0500

    First simple build

---
+++ Assets/LightController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    Texture2D texture;
+    float width;
+    public float lightDistance;
+    SpriteRenderer ren;
+    Sprite sprite;
+    public float angle;
+    public float rotation;
+
+    void OnValidate()
+    {
+        ren = GetComponent<SpriteRenderer>();
+        UpdateCone();
+        UpdateShape();
+        Rotate();
+    }
+
+    void Awake()
+    {
+        ren = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        UpdateCone();
+        UpdateShape();
+        Rotate();
+    }
+
+    void UpdateShape()
+    {
+        if (transform.localScale.x == width && transform.localScale.y == lightDistance) { return; }
+        transform.localScale = new Vector3(width, lightDistance, 1f);
+    }
+
+    void UpdateCone()
+    {
+        float angleRad = (angle / 2f) * Mathf.PI / 180f;
+        width = Mathf.Sin(angleRad) * lightDistance / Mathf.Cos(angleRad);
+    }
+
+    void Rotate()
+    {
+        if (rotation == transform.eulerAngles.z) { return; }
+        Vector3 temp = transform.eulerAngles;
+        temp.z = rotation;
+        transform.eulerAngles = temp;
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float moveSpeed = 10f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        float velocity = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        rb.velocity = new Vector2(velocity, rb.velocity.y);
+    }
+}
