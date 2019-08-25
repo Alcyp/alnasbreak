f1b88ee2f87e527e8e3050cf44e9e00349c13520
Kenia <kenia1001@yahoo.com.mx>
2019-08-24 20:07:39 -0500

    Mirror! Music! Meow!
    😼😼😼

---
+++ Assets/Scripts/RotateGlow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGlow : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, 0f, rotationSpeed * Time.deltaTime));
+    }
+}
